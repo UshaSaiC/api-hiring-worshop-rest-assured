430a9bf4e4658d53b16343898047a5651a930688
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-01-08 09:24:39 +0530

    Added Tests

--- src/test/java/GetSingleUser.java
+++ src/test/java/GetSingleUser.java
@@ -1,15 +1,22 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.comparesEqualTo;
 
 public class GetSingleUser {
+
     @Test
-    public void testGetSingleUser() {
-        given().
-                when().get("https://reqres.in/api/users/2").then().
-                assertThat().statusCode(200).
-                body("data.id", comparesEqualTo(2));
+    public void testGetSingleUserandExtractResponse(){
+        Response response=given().contentType(ContentType.JSON).when().get("https://reqres.in/api/users/2").then().
+                assertThat().statusCode(200).and().extract().response();
+    System.out.println("response of the list user API is  "+response.asString());
     }
 
+    @Test
+    public void testGetSingleUser(){
+        given().contentType(ContentType.JSON).when().get("https://reqres.in/api/users/2").then().
+                assertThat().statusCode(200).body("data.id", comparesEqualTo(2));
+    }
 }
